ea5306bf250d9fad453db943db8b489018f32cb6
miguelemosreverte <miguellemos95@gmail.com>
2018-01-30 17:50:18 -0300

    Added a default C file to parse

---
+++ code examples/test.c
@@ -0,0 +1,27 @@
+int _x = 1.8;
+double exec (double);
+
+int main (void) {
+    double y = 1, z = 3.5;
+    for (_x = 5; _x < 10; _x = _x + 1)
+        y = exec(y) + exec(z);
+    if (y < 100) {
+        y = y / 2;
+        if (a == 0){}
+    }
+    else
+        z = y + 5.5;
+
+    return 0;
+}
+double exec (double asd){
+double exec (double valor) {
+    int i = 10;
+
+    while ((i >= 5) && (valor > 3.4))
+        valor -= _x - i;
+
+    return valor;
+}
+}
+        
