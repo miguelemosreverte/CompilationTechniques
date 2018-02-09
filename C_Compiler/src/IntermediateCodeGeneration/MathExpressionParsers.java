800d1339b3bda2eb707c30d5250c5170304b13dd
miguelemosreverte <miguellemos95@gmail.com>
2018-02-09 00:35:39 -0300

    Okay. So I remember just an hour ago saying that using bigger information in the parameters would help solve the problem. That was a good step, in the right direction.
    Now, what I actually did was to implement each of the four cases independently inside the parseSum function.
    
    case 1: there is a sum of two non-groups.
    case 2: there is a sum of a left group and a right final
    case 3: there is a sum of a left final and a right group
    case 4: there is a sum of a left group and a right group
    
    This allowed to turn this expression:
    9 + ((1 * 3) + (2* 3)) + 5
    
    into this:
    
    t1:=2*3
    t2:=1*3
    t3:=t2+t1
    t4:=9+t3
    t5:=9+5

--- C_Compiler/src/IntermediateCodeGeneration/MathExpressionParsers.java
+++ C_Compiler/src/IntermediateCodeGeneration/MathExpressionParsers.java
@@ -7,6 +7,8 @@ package IntermediateCodeGeneration;
 
 import C_ANTLR.CParser;
 import C_ANTLR.CParser.ProductContext;
+import java.util.ArrayList;
+import java.util.List;
 import org.antlr.v4.runtime.tree.TerminalNode;
 
 /**
@@ -134,84 +136,171 @@ public class MathExpressionParsers {
         return tuple;
     }    
     
-    public static boolean willRequireExtraCalculations(CParser.ProductContext product){        
-        return product.factor().math_operand().grouped() == null || product.unapplied_medium_op().isEmpty();
+    public static boolean willRequireExtraCalculations(CParser.ProductContext product){  
+       return product.factor().math_operand().grouped() != null || !product.unapplied_medium_op().isEmpty();
     } 
         
     public static MathTuple<String,Integer> 
         parseSum(
             MathTuple<String,Integer> tuple, 
-            CParser.SumContext sum){
+            CParser.SumContext sum,
+            List<CParser.Unapplied_low_opContext>shrinkableList){
         
         
+            
+            
+            
+        ProductContext operand1 = sum.product();
+        
+        //case 0
+        // there is just a product
         if (sum.unapplied_low_op().isEmpty()){
-            System.out.println("Oops");
             tuple = parseProduct(tuple, sum.product());
-            return tuple; //not really necessary, but helps posterior analysis
+            return tuple;
+        }
+        
+        //from here on we can assume there are unapplied low operations to work with
+        
+        //after taking what we are going to use, we remove it from the list
+        //for this I needed a shrinkable list because sum.unapplied_low_op() 
+        //would return a constant value.
+        if (shrinkableList.isEmpty()) shrinkableList = sum.unapplied_low_op();
+        
+        CParser.Unapplied_low_opContext unapplied_low_op = shrinkableList.remove(0);  
+        ProductContext operand2 = unapplied_low_op.product();
+        String operation = unapplied_low_op.MATH_OP_LOW_PRIORITY().getText();
+        
+        
+        
+        
+        
+        /*
+        
+        Just a bit of trivia, before continuing:
+        
+        non-group is the same as final. Means a non-recursive math operand.
+        
+        This means that it is expected for that value to be processed into a single operand.
+        
+        A recursive math operand means that it is expected to become N extra steps.
+        
+        */
+        
+        
+        
+        
+        //case 1 
+        // there is a sum of two non-groups. 
+        
+        if (! willRequireExtraCalculations(operand1) &&
+            ! willRequireExtraCalculations(operand2)){ 
+        
+            
+            tuple = tuple.set_ID(tuple.ID + 1);
+            tuple = tuple.set_acumulatedIntermediateCode("\nt" + tuple.ID + ":=");
+            
+            tuple = parseProduct(tuple, operand1);
+            Integer operand1_ID = tuple.ID;
+            
+            tuple = tuple.set_acumulatedIntermediateCode(operation);
+            
+            tuple = parseProduct(tuple, operand2);
+            Integer operand2_ID = tuple.ID;
+            
+        
         }
         
         
         
-        if (! willRequireExtraCalculations(sum.product())){               
-                tuple = tuple.set_acumulatedIntermediateCode("\ntaa" + tuple.ID + ":=");
-        }
-        else{
-
-            if (sum.product().factor().math_operand().grouped() == null){
-                //if it is a group we are going to be back here in a minute, 
-                //so no need to print this twice. 
-                // grouped is the only recursive math operand.
-
-                tuple = tuple.set_ID(tuple.ID + 1);
-                tuple = tuple.set_acumulatedIntermediateCode("\ntbb" + tuple.ID + ":=");
-            }
-            tuple = parseProduct(tuple, sum.product());
-        }
+        
+        //case 2
+        // there is a sum of a left group and a right final
+        
+        if (  willRequireExtraCalculations(operand1) &&
+            ! willRequireExtraCalculations(operand2)){ 
+        
             
-        TerminalNode groupLowOperand = null;
+            
+            tuple = parseProduct(tuple, operand1);
+            Integer operand1_ID = tuple.ID;
+            
+            
+            tuple = tuple.set_ID(tuple.ID + 1);
+            tuple = tuple.set_acumulatedIntermediateCode("\nt" + tuple.ID + ":=" + "t" + operand1_ID);
+            
+            tuple = tuple.set_acumulatedIntermediateCode(operation);
+            
+            tuple = parseProduct(tuple, operand2);
+            Integer operand2_ID = tuple.ID;
+            
+        
+        }
+        
+            
         
         
-        boolean needs_prefix = willRequireExtraCalculations(sum.product());
-        for (CParser.Unapplied_low_opContext low_op : sum.unapplied_low_op()){
+        
+         //case 3
+        // there is a sum of a left final and a right group
+        
+        if (!  willRequireExtraCalculations(operand1) &&
+               willRequireExtraCalculations(operand2)){ 
+        
             
             
-           
+            
+            tuple = parseProduct(tuple, operand2);
+            Integer operand2_ID = tuple.ID;           
+            
+            
+            tuple = tuple.set_ID(tuple.ID + 1);
+            tuple = tuple.set_acumulatedIntermediateCode("\nt" + tuple.ID + ":=");
+            
+            tuple = parseProduct(tuple, operand1);
+            Integer operand1_ID = tuple.ID;
+            
+            tuple = tuple.set_acumulatedIntermediateCode(operation);
+            
+            tuple = tuple.set_acumulatedIntermediateCode("t" + operand2_ID);
+        
+        }
+        
+        
+        
+        
           
+         //case 4
+        // there is a sum of a left group and a right group
+        
+        if (  willRequireExtraCalculations(operand1) &&
+              willRequireExtraCalculations(operand2)){ 
+        
             
             
             
+            tuple = parseProduct(tuple, operand2);
+            Integer operand2_ID = tuple.ID;           
             
-            Integer pepe = 0;
             
-            if (! willRequireExtraCalculations(low_op.product())){                
-                tuple = tuple.set_acumulatedIntermediateCode("\ntd" + tuple.ID + ":=");
-            }
-            else{
-                
-                
-                if (low_op.product().factor().math_operand().grouped() != null)  {
-                    groupLowOperand = low_op.MATH_OP_LOW_PRIORITY();
-                    tuple = parseProduct(tuple, low_op.product());
-                    pepe = tuple.ID;
-                }
-                else{
-                     if (!needs_prefix){
-                        needs_prefix = true;
-                    }
-                    else{
-                        tuple = tuple.set_ID(tuple.ID + 1);
-                        tuple = tuple.set_acumulatedIntermediateCode("\ntc" + tuple.ID + ":=t" + (tuple.ID - 1));
-                        // tc is waiting for a unnaplied. And that may not happen. It is waiting for a resolved product on the right.
-                    }
-                    tuple = tuple.set_acumulatedIntermediateCode(low_op.MATH_OP_LOW_PRIORITY().getText());
-                    tuple = parseProduct(tuple, low_op.product());
-                    
-                }
-            }
+            tuple = parseProduct(tuple, operand1);
+            Integer operand1_ID = tuple.ID;
             
-             
+            tuple = tuple.set_ID(tuple.ID + 1);
+            tuple = tuple.set_acumulatedIntermediateCode("\nt" + tuple.ID + ":=");
+            
+            tuple = tuple.set_acumulatedIntermediateCode("t" + operand1_ID);            
+            
+            tuple = tuple.set_acumulatedIntermediateCode(operation);
+            
+            tuple = tuple.set_acumulatedIntermediateCode("t" + operand2_ID);
         
         }
+        
+        if (!shrinkableList.isEmpty()){
+            System.out.println(sum.unapplied_low_op().get(0).getText());
+            tuple = parseSum(tuple, sum, shrinkableList);
+        }
+        
         
         return tuple;
     }
@@ -222,7 +311,8 @@ public class MathExpressionParsers {
             MathTuple<String,Integer> tuple, 
             CParser.Math_operationContext math_expression){       
         
-        return parseSum(tuple, math_expression.sum());
+            List shrinkableList = new ArrayList<>();
+        return parseSum(tuple, math_expression.sum(), shrinkableList);
     }
     
 }
